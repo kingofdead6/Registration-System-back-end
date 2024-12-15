4d4d0402d70dfac7321535f7c713889060ff8edc
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2024-12-15 11:18:09 +0100

    Update web.php

--- event-manager-api-v1/routes/web.php
+++ event-manager-api-v1/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Mail\AcceptanceMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $recipientEmail = 'na_bougroura@esi.dz';
+    $recipientName = 'na_bougroura';
+
+    Mail::to($recipientEmail)->send(new AcceptanceMail($recipientName));
 });
